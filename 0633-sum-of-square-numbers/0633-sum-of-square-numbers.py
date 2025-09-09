7f1bff2e0f0f99e5a4d15dee5d8928485297b7e8
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-09-09 18:16:00 +0530

    Time: 95 ms (21.65%), Space: 17.8 MB (48.6%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,6 +3,10 @@ class Solution:
         s,e = 0,int(c**0.5)+1
         while s <= e:
             curr = s*s + e*e
+            if 2*(s*s) == c:
+                return True
+            if 2*(e*e) == c:
+                return True
             if curr == c:
                 return True
             elif curr > c:
