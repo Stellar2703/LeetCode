36768684e6ab377b9d8e5259abfcfbb45f4c56c5
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-10-02 12:09:06 +0530

    Time: 13 ms (55.54%), Space: 20.9 MB (55.71%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    # def anagram(self, i: str, j: str) -> bool:
+    #     # Check if both words are anagrams
+    #     return sorted(i) == sorted(j)
+        
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     res = []
+    #     visited = [False] * len(strs)  # To avoid reusing words
+
+    #     for i in range(len(strs)):
+    #         if visited[i]:
+    #             continue
+    #         sol = [strs[i]]
+    #         visited[i] = True
+    #         for j in range(i + 1, len(strs)):
+    #             if not visited[j] and self.anagram(strs[i], strs[j]):
+    #                 sol.append(strs[j])
+    #                 visited[j] = True
+    #         res.append(sol)
+    #     return res
+        ans = defaultdict(list)
+        for i in strs:
+            ans[''.join(sorted(i))].append(i)
+        return list(ans.values())
+            
+            
