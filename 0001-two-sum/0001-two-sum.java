bb332f0dcda09f6fd166a4770c6bf370ee0ffef1
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-17 20:01:43 +0530

    Time: 2 ms (99.11%), Space: 47.4 MB (11.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,10 @@ class Solution {
         HashMap<Integer,Integer> mp = new HashMap<>();
         for (int i = 0 ; i < nums.length ; i++){
             int curr = nums[i];
-            int remaining  =  target - curr;
-            if (mp.containsKey(remaining)){
+            // int remaining  =  target - curr;
+            if (mp.containsKey(target - curr)){
                 ans[0] = i;
-                ans[1] = mp.get(remaining);
+                ans[1] = mp.get(target - curr);
                 return ans;
             }
             mp.put(nums[i],i);
