91881727b09bf6fcc6faeabcd8e385104eba2179
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-18 10:04:17 +0530

    Time: 0 ms (100%), Space: 19 MB (99.91%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,14 @@
 from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        b = Counter(nums)
-        # print(b)
-        for i in b:
-            if b[i] == 1:
-                return i
+        # b = Counter(nums)
+        # # print(b)
+        # for i in b:
+        #     if b[i] == 1:
+        #         return i
         
+
+        xor = nums[0]
+        for i in range(1,len(nums)):
+            xor = xor^nums[i]
+        return xor
