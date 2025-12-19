38d5bbf421220f0756b6ac324c4c8ef53813a127
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-19 13:59:32 +0530

    Time: 0 ms (100%), Space: 47.7 MB (39.84%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,42 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int [] prefix = new int[n];
-        int [] suffix = new int[n];
-        prefix[0]  = height [0];
-        for (int i=1;i<n;i++){
-            prefix[i] = Math.max(prefix[i-1],height[i]);
-        }
-        suffix[n-1]  = height [n-1];
-        for (int i=n-2;i>=0;i--){
-            suffix[i] = Math.max(suffix[i+1],height[i]);
-        }
-        int total = 0;
-        for (int i =0;i<n;i++){
-            total+= (Math.min(prefix[i],suffix[i]))-height[i];
-        }
+        // int n = height.length;
+        // int [] prefix = new int[n];
+        // int [] suffix = new int[n];
+        // prefix[0]  = height [0];
+        // for (int i=1;i<n;i++){
+        //     prefix[i] = Math.max(prefix[i-1],height[i]);
+        // }
+        // suffix[n-1]  = height [n-1];
+        // for (int i=n-2;i>=0;i--){
+        //     suffix[i] = Math.max(suffix[i+1],height[i]);
+        // }
+        // int total = 0;
+        // for (int i =0;i<n;i++){
+        //     total+= (Math.min(prefix[i],suffix[i]))-height[i];
+        // }
 
+        // return total;
+
+
+
+        int n = height.length;
+        int l = 0 , r = n-1 , total = 0 , right = 0 , left = 0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(left>height[l]){
+                    total += (left-height[l]);
+                }
+                left = Math.max(left,height[l]);
+                l++;
+            }else{
+                if(right > height[r]){
+                    total+= (right-height[r]);
+                }
+                right = Math.max(right,height[r]);
+                r--;
+            }
+        }
         return total;
+        }
     }
-}
