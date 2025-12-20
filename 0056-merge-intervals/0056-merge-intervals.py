fa9d17f288e3b4434d2d8b16031788fb02a69fc2
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-20 21:14:23 +0530

    Time: 11 ms (30.36%), Space: 20.3 MB (99.92%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, n: List[List[int]]) -> List[List[int]]:
+        ans = []
+        n.sort(key=lambda x: x[0]) 
+        ans.append(n[0])
+        print(n)
+        ptr = 1
+        for i in range(1,len(n)):
+            if (ans[ptr-1][1] >= n[i][0]):
+                # ans[ptr-1] = [ans[ptr-1][0],n[i][1]]
+                ans[ptr-1] = [min(n[i][0],ans[ptr-1][0]),max(n[i][1],ans[ptr-1][1])]
+            else:
+                ans.append(n[i])
+                ptr+=1
+        return ans
+
+            
+    
