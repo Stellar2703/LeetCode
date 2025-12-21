c52abda5a277afb5fe6b746fb5a588b015d03697
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-21 19:10:56 +0530

    Time: 19 ms (65.08%), Space: 17.4 MB (99.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        window = [0]*26
-        given = [0] *26
+        # window = [0]*26
+        # given = [0] *26
 
-        if len(s1) > len(s2):
-            return False
+        # if len(s1) > len(s2):
+        #     return False
 
-        for i in range (len(s1)):
-            given[ord(s1[i])-ord('a')]+=1
+        # for i in range (len(s1)):
+        #     given[ord(s1[i])-ord('a')]+=1
         
-        for i in range (len(s1)):
-            window[ord(s2[i])-ord('a')]+=1
+        # for i in range (len(s1)):
+        #     window[ord(s2[i])-ord('a')]+=1
 
-        if given == window:
-            return True
+        # if given == window:
+        #     return True
 
-        for i in range(len(s1),len(s2)):
-            window[ord(s2[i])-ord('a')]+=1
-            window[ord(s2[i-len(s1)])-ord('a')]-=1
-            if given == window:
+        # for i in range(len(s1),len(s2)):
+        #     window[ord(s2[i])-ord('a')]+=1
+        #     window[ord(s2[i-len(s1)])-ord('a')]-=1
+        #     if given == window:
+        #         return True
+        # return False
+        giv = {}
+        ans = {}
+        count =0
+        for i in range(len(s1)):
+            giv[s1[i]]  = giv.get(s1[i],0)+1
+        l = 0
+        for r in range(len(s2)):
+            ans[s2[r]]  = ans.get(s2[r],0)+1
+            
+            if (r-l+1>len(s1)):
+                ans[s2[l]] -=1
+                if(ans[s2[l]]==0):
+                    del ans[s2[l]]
+                l+=1
+            if giv == ans:
                 return True
         return False
+            
+            
 
