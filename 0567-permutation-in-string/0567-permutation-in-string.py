fd9627781e20b122f04b2044aac71e01cbc9ef4b
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-21 19:12:47 +0530

    Time: 20 ms (55.57%), Space: 17.6 MB (98.18%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,7 +8,7 @@ class Solution:
 
         # for i in range (len(s1)):
         #     given[ord(s1[i])-ord('a')]+=1
-        
+
         # for i in range (len(s1)):
         #     window[ord(s2[i])-ord('a')]+=1
 
@@ -23,21 +23,18 @@ class Solution:
         # return False
         giv = {}
         ans = {}
-        count =0
+        count = 0
         for i in range(len(s1)):
-            giv[s1[i]]  = giv.get(s1[i],0)+1
+            giv[s1[i]] = giv.get(s1[i], 0) + 1
         l = 0
         for r in range(len(s2)):
-            ans[s2[r]]  = ans.get(s2[r],0)+1
-            
-            if (r-l+1>len(s1)):
-                ans[s2[l]] -=1
-                if(ans[s2[l]]==0):
+            ans[s2[r]] = ans.get(s2[r], 0) + 1
+
+            if r - l + 1 > len(s1):
+                ans[s2[l]] -= 1
+                if ans[s2[l]] == 0:
                     del ans[s2[l]]
-                l+=1
+                l += 1
             if giv == ans:
                 return True
         return False
-            
-            
-
