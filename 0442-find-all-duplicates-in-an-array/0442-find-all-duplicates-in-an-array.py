0d46c574f34e8b4db4879d69297331dbb05438a4
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-23 21:22:17 +0530

    Time: 49 ms (44.03%), Space: 19.9 MB (55.63%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,29 +1,23 @@
-# class Solution(object):
-#     def findDuplicates(self, nums):
-        # """
-        # :type nums: List[int]
-        # :rtype: List[int]
-        # """
-        # l = 0
-        # while (l<len(nums)):
-        #     if(nums[l]!= nums[nums[l]-1]):
-        #         nums[l],nums[nums[l]-1] = nums[nums[l]-1], nums[l]
-        #     else:
-        #         l+=1
-        # return nums
 class Solution:
     def findDuplicates(self, nums):
-        i = 0
-        while i < len(nums):
-            correct = nums[i] - 1
-            if nums[i] != nums[correct]:
-                nums[i], nums[correct] = nums[correct], nums[i]
-            else:
-                i += 1
-
-        res = []
+        # i = 0
+        # while i < len(nums):
+        #     correct = nums[i] - 1
+        #     if nums[i] != nums[correct]:
+        #         nums[i], nums[correct] = nums[correct], nums[i]
+        #     else:
+        #         i += 1
+        # res = []
+        # for i in range(len(nums)):
+        #     if nums[i] != i + 1:
+        #         res.append(nums[i])
+        # return res
+        ans = []
         for i in range(len(nums)):
-            if nums[i] != i + 1:
-                res.append(nums[i])
+            correct = abs(nums[i]) - 1
+            if nums[correct] < 0:
+                ans.append(correct + 1)
+            else:
+                nums[correct] = -nums[correct]
+        return ans
 
-        return res
