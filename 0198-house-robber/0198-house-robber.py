993312d81a7d82a03ac57c651cdf075433504ee7
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-24 14:00:55 +0530

    Time: 0 ms (100%), Space: 17.1 MB (99.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -11,15 +11,52 @@ class Solution:
         # n = len(nums)
         # ans = rob(n-1,nums)
         # return ans
-        def f(index,nums,dp):
-            if index == 0 : return nums[0]
-            if index<0 : return 0
-            if dp[index] != -1: return dp[index]
-            pick = nums[index] + f(index-2,nums,dp)
-            not_pick = f(index-1,nums,dp)
-            dp[index] = max(pick,not_pick)
-            return dp[index]
+
+
+
+        #### Memoization #####
+        # def f(index,nums,dp):
+        #     if index == 0 : return nums[0]
+        #     if index<0 : return 0
+        #     if dp[index] != -1: return dp[index]
+        #     pick = nums[index] + f(index-2,nums,dp)
+        #     not_pick = f(index-1,nums,dp)
+        #     dp[index] = max(pick,not_pick)
+        #     return dp[index]
+        # n = len(nums)
+        # dp = [-1] * (len(nums))
+        # ans = f(n-1,nums,dp)
+        # return ans
+
+
+        ### Tabulation ###
+        # n = len(nums)
+        # dp = [-1] * n
+        # dp[0]  = nums[0]
+
+        # for i in range(1,n):
+        #     not_pick = dp[i-1]
+        #     pick = nums[i]
+        #     if i>1:
+        #         pick+=dp[i-2]
+        #     dp[i] = max(pick,not_pick)
+        # return dp[n-1]
+
+
+
+        ### Space Optimization ###
+
         n = len(nums)
-        dp = [-1] * (len(nums))
-        ans = f(n-1,nums,dp)
-        return ans
+        
+        prev  = nums[0]
+        prev2 = 0
+
+        for i in range(1,n):
+            not_pick = prev
+            pick = nums[i]
+            if i>1:
+                pick+=prev2
+            curr = max(pick,not_pick)
+            prev2 = prev
+            prev = curr
+        return prev
