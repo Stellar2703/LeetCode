5fe62e2ec71d68bd73377791076ee080dc4a4872
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2025-12-25 12:21:41 +0530

    Time: 0 ms (100%), Space: 12.5 MB (51.08%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        while num >= 10:
-            num =sum([int(x) for x in str(num)])
-        return num
+        # while num >= 10:
+        #     num =sum([int(x) for x in str(num)])
+        # return num
+        if num == 0:
+            return 0
+        return ((num-1)% 9 )+1
