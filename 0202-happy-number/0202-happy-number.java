be9f777ebd5652c66a1f6c38333b696a0ac3ef4d
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-02 22:51:20 +0530

    Time: 1 ms (78.87%), Space: 42.7 MB (11.31%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,16 +1,36 @@
 class Solution {
-    public boolean isHappy(int n) {
-        HashSet<Integer> mp = new HashSet<>();
-        while(n!=1 && !mp.contains(n)){
-            mp.add(n);
-            int temp = 0;
-            while (n>0){
-                temp +=((n%10)*(n%10));
-                n= n/10;
-            }
-            n = temp;
+    int f(int n) {
+        int temp = 0;
+        while (n > 0) {
+            temp += ((n % 10) * (n % 10));
+            n = n / 10;
         }
-        if (n==1) return true;
-        return false;
+        return temp;
+    }
+
+    public boolean isHappy(int n) {
+        //     HashSet<Integer> mp = new HashSet<>();
+        //     while(n!=1 && !mp.contains(n)){
+        //         mp.add(n);
+        //         int temp = 0;
+        //         while (n>0){
+        //             temp +=((n%10)*(n%10));
+        //             n= n/10;
+        //         }
+        //         n = temp;
+        //     }
+        //     if (n==1) return true;
+        //     return false;
+        // }
+        int slow = n, fast = n;
+        while (n != 1) {
+            slow = f(slow);
+            fast = f(f(fast));
+            if (fast == 1)
+                return true;
+            if (slow == fast)
+                return false;
+        }
+        return true;
     }
 }
