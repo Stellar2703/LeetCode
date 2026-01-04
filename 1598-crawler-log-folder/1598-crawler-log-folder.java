32f0f36cca5fbabc686a0e413ac315974b20e7ee
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-04 22:52:17 +0530

    Time: 1 ms (99.77%), Space: 43.9 MB (59.16%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -2,11 +2,13 @@ class Solution {
     public int minOperations(String[] logs) {
         int count =0;
        for(String s : logs){
-        if (s.equals("../")) count--;
+        if (s.equals("../")) {
+            if(count>0) count--;
+        }
         else if(s.equals("./")){}
         else count ++;
        }
-        return count>0 ? count : 0;
+        return count;
         
     }
 }
