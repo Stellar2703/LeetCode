52b8f5e4999ce828190c0eb7587ca48017b85173
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-06 10:19:07 +0530

    Time: 1 ms (100%), Space: 47.6 MB (15.82%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,20 +1,39 @@
-            class Solution {
-                public int[] resultsArray(int[] nums, int k) {
-                    int r = k-1,l=0;
-                    boolean is_valid = true;
-                    int [] ans = new int[nums.length-k+1];
-                    while(r<nums.length){
-                        for(int i = l+1;i<=r;i++){
-                            if(nums[i-1]+1 != nums[i]){
-                                is_valid = false;
-                                break;
-                            }
-                        }
-                        if(is_valid) ans[l++] = nums[r];
-                        else ans[l++] = -1;
-                        is_valid = true; 
-                        r++;
-                    }
-                    return ans;
-                }
-            }
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int count = 0 , l=0;
+        int [] ans = new int [nums.length-k+1];
+        for (int i = 1;i<k;i++){
+            if(nums[i-1]+1==nums[i]) count++;
+        }
+        ans[l++] = (count == k-1) ? nums[k-1] : -1;
+        for(int r = k;r<nums.length;r++){
+            if(nums[l-1]+1==nums[l]) count--;
+            if(nums[r-1]+1==nums[r]) count++;
+            ans[l++] = (count==k-1) ? nums[r] : -1;
+        }
+    return ans;
+    }
+}
+
+
+
+// class Solution {
+    // public int[] resultsArray(int[] nums, int k) {
+        // int r = k-1,l=0;
+        // boolean is_valid = true;
+        // int [] ans = new int[nums.length-k+1];
+        // while(r<nums.length){
+            // for(int i = l+1;i<=r;i++){
+                // if(nums[i-1]+1 != nums[i]){
+                    // is_valid = false;
+                    // break;
+                // }
+            // }
+            // if(is_valid) ans[l++] = nums[r];
+            // else ans[l++] = -1;
+            // is_valid = true; 
+            // r++;
+        // }
+        // return ans;
+    // }
+// }
