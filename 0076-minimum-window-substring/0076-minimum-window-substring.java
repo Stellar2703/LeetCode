0c9f6f490dc2dc81868c719d18183bc52fefae38
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-06 20:36:53 +0530

    Time: 19 ms (47.78%), Space: 48 MB (17.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,32 +2,33 @@ class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character,Integer> need = new HashMap<>();
         HashMap<Character,Integer> present = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            need.put(c, need.getOrDefault(c, 0) + 1);
+        int l=0,r=0,min=Integer.MAX_VALUE,formed = 0,start =0;
+        for(int i =0;i<t.length();i++){
+            char temp= t.charAt(i);
+            need.put(temp,need.getOrDefault(temp,0)+1);
         }
         int req = need.size();
-        int l = 0,min = Integer.MAX_VALUE,formed = 0,start=0,r=0;
+
         while(r<s.length()){
-            char comp = s.charAt(r);
-            present.put(comp,present.getOrDefault(comp,0)+1);
-            if(need.containsKey(comp)&& present.get(comp)==need.get(comp)){
-                formed++;
-            }
-            while(formed==req){
+            char right = s.charAt(r);
+            present.put(right,present.getOrDefault(right,0)+1);
+            if(need.containsKey(right) && present.get(right).intValue()==need.get(right).intValue()) formed++;
+
+            while(formed == req){
                 if(min>r-l+1){
                     min = r-l+1;
                     start = l;
                 }
                 char left = s.charAt(l);
                 present.put(left,present.get(left)-1);
-                if(need.containsKey(left) && need.get(left)>present.get(left)){
+                if(need.containsKey(left)&&present.get(left)<need.get(left)){
                     formed--;
                 }
-                l++;
+                    l++;
             }
             r++;
-        }   
-        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
-
         }
+
+            return min == Integer.MAX_VALUE ? "" : s.substring(start,start+min);
     }
+}
