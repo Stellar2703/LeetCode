5787ebca5fabfdd0f5d5528b7c4478b9cbe7d42e
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-07 09:29:21 +0530

    Time: 11 ms (54.88%), Space: 46.2 MB (66.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+//         HashMap<Character,Integer> mp = new HashMap<>();
+//         int l=0,freq=0,ans = 0;
+//         for(int r=0;r<s.length();r++){
+//             char right = s.charAt(r);
+//             mp.put(right,mp.getOrDefault(right,0)+1);
+//             freq = Math.max(freq,mp.get(right));
+//             if(r-l+1-freq<=k){
+//                 ans = Math.max(r-l+1,ans);
+//             }
+//             char left = s.charAt(l);
+//             if(r-l+1-freq>k){
+//                 mp.put(left,mp.get(left)-1);
+//                 l++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character,Integer> mp = new HashMap<>();
-        int l=0,freq=0,ans = 0;
-        for(int r=0;r<s.length();r++){
-            char right = s.charAt(r);
-            mp.put(right,mp.getOrDefault(right,0)+1);
-            freq = Math.max(freq,mp.get(right));
-            if(r-l+1-freq<=k){
-                ans = Math.max(r-l+1,ans);
-            }
-            char left = s.charAt(l);
-            if(r-l+1-freq>k){
-                mp.put(left,mp.get(left)-1);
+        int[] count = new int[26];
+        int l = 0, maxFreq = 0, ans = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            int idx = s.charAt(r) - 'A';
+            count[idx]++;
+            maxFreq = Math.max(maxFreq, count[idx]);
+
+            if (r - l + 1 - maxFreq > k) {
+                count[s.charAt(l) - 'A']--;
                 l++;
             }
+
+            ans = Math.max(ans, r - l + 1);
         }
         return ans;
     }
