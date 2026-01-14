4372dfcd73b68975b3ae2dbbf75979ad16258680
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-14 20:44:56 +0530

    Time: 1 ms (73.13%), Space: 43.5 MB (72.85%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -21,15 +21,12 @@ class Solution {
         q.offer(root);
         while(!q.isEmpty()){
             int size = q.size();
-            List<Integer> temp = new ArrayList<>();
             for(int i =0;i<size;i++){
                 TreeNode node = q.poll();
                 if(node.left!=null) q.offer(node.left);
                 if(node.right!=null) q.offer(node.right);
-                temp.add(node.val);
+                if (i == size - 1) ans.add(node.val); // rightmost
             }
-            ans.add(temp.get(temp.size()-1));
-            // System.out.print(temp[0]);
         }
         return ans;
     }   
