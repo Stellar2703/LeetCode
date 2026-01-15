a43332a0610cdcdfe8d61577a23110a0a4513875
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-15 14:35:14 +0530

    Time: 0 ms (100%), Space: 46.8 MB (81.95%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -13,24 +13,47 @@
  *     }
  * }
  */
+
+
+//  class Solution {
+//     public List<Integer> largestValues(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         int max = Integer.MIN_VALUE;
+//         if(root == null) return ans;
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             max = Integer.MIN_VALUE;
+//             for(int i=0;i<size;i++){
+//                 TreeNode node = q.poll();
+//                 if(node.left!= null) q.offer(node.left);
+//                 if(node.right!= null) q.offer(node.right);
+//                 if(node.val > max) max = node.val;
+//             }
+//             ans.add(max);
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        int max = Integer.MIN_VALUE;
-        if(root == null) return ans;
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            max = Integer.MIN_VALUE;
-            for(int i=0;i<size;i++){
-                TreeNode node = q.poll();
-                if(node.left!= null) q.offer(node.left);
-                if(node.right!= null) q.offer(node.right);
-                if(node.val > max) max = node.val;
-            }
-            ans.add(max);
-        }
+        dfs(root,0,ans);
         return ans;
+    }
+
+    private void dfs(TreeNode root,int level,List<Integer> ans ){
+        if(root==null) return;
+        if(ans.size()==level){
+            ans.add(root.val);
+        }
+        else{
+            ans.set(level, Math.max(ans.get(level), root.val));
+        }
+        dfs(root.left,level+1,ans);
+        dfs(root.right,level+1,ans);
     }
 }
