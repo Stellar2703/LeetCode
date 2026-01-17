35592f41876f996e21ef6265650c52b96981703c
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-17 17:20:21 +0530

    Time: 0 ms (100%), Space: 42.8 MB (76.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,10 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p== null && q==null) return true;
-        if(p== null || q== null) return false;
-        if(p.val!=q.val) return false;
+        return preorder(p,q);
+    }
+    private Boolean preorder(TreeNode p, TreeNode q){
+        if(p == null && q==null) return true;
+        if(p== null || q==null) return false;
+        if(p.val != q.val) return false;
 
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return preorder(p.left,q.left)&&preorder(p.right,q.right);
     }
 }
