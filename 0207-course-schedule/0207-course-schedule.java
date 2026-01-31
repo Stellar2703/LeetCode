9c652f6e3ed1f2574c8942062a28c6e995634398
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-31 22:08:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,31 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        int n = numCourses;
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        int [] indeg = new int[numCourses];
-        for(int [] edge : prerequisites){
-            int a = edge[0];
-            int b = edge[1];
-            adj.get(b).add(a);
-            indeg[a]++;
+        int[] indeg = new int[n];
+        for(int[] edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(v).add(u);
+            indeg[u]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i =0;i<numCourses;i++){
-            if (indeg[i]==0) q.offer(i);
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.offer(i);
+            }
         }
+        int count =0;
         while(!q.isEmpty()){
             int node = q.poll();
-            ans.add(node);
-            for(int a : adj.get(node)){
+            count++;
+            for(int a: adj.get(node)){
                 indeg[a]--;
                 if(indeg[a]==0){
-                    q.offer(a);
+                    q.offer(indeg[a]);
                 }
             }
         }
-            return ans.size() == numCourses ? true : false;
-        }
+        return count == n ? true : false;
     }
+}
