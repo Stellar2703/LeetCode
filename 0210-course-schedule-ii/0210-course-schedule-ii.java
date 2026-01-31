085ac439f929d0e8735bb4848c4877b7c936005c
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-01-31 22:26:40 +0530

    Time: 102 ms (5.26%), Space: 46.7 MB (79.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -2,38 +2,39 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prereq) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = numCourses;
-        for(int i =0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        int [] indeg = new int[n];
-        for(int[] edge : prereq){
+        int[] indeg = new int[n];
+        for (int[] edge : prereq) {
             int u = edge[0];
             int v = edge[1];
             adj.get(v).add(u);
             indeg[u]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
+        for (int i = 0; i < n; i++) {
+            if (indeg[i] == 0) {
                 q.offer(i);
             }
         }
 
-        ArrayList<Integer> ans =new ArrayList<>();
-        while(!q.isEmpty()){
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!q.isEmpty()) {
             int node = q.poll();
             ans.add(node);
-            for(int a : adj.get(node)){
+            for (int a : adj.get(node)) {
                 indeg[a]--;
-                if(indeg[a]==0){
+                if (indeg[a] == 0) {
                     q.offer(a);
                 }
             }
         }
-        if(ans.size() != n) return new int[0];
+        if (ans.size() != n)
+            return new int[0];
         int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            result[i]=ans.get(i);
+        for (int i = 0; i < n; i++) {
+            result[i] = ans.get(i);
         }
         return result;
     }
