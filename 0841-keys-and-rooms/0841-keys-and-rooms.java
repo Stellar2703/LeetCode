20a4d55dee1523536fd7e9de828ed3504d32bac4
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-01 12:34:24 +0530

    Time: 1 ms (84.55%), Space: 46.6 MB (23.27%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,20 +1,41 @@
 class Solution {
+    public void dfs(int index,List<List<Integer>> rooms,int[] vis){
+        vis[index]  = 1;
+        for(int a : rooms.get(index)){
+            if(vis[a]==0) dfs(a,rooms,vis);
+        }
+    }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int [] vis = new int[rooms.size()];
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
-        int count = 1;
-        vis[0] =1;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int a : rooms.get(node)){
-                if(vis[a]==0){
-                    vis[a]=1;
-                    count++;
-                    q.offer(a);
-                }
-            }
+        dfs(0,rooms,vis);
+        for(int i=0;i<rooms.size();i++){
+            if(vis[i]==0) return false;
         }
-        return count == rooms.size();
+        return true;
     }
 }
+
+
+
+// BFS Approach
+
+// class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         int [] vis = new int[rooms.size()];
+//         Queue<Integer> q = new LinkedList<>();
+//         q.offer(0);
+//         int count = 1;
+//         vis[0] =1;
+//         while(!q.isEmpty()){
+//             int node = q.poll();
+//             for(int a : rooms.get(node)){
+//                 if(vis[a]==0){
+//                     vis[a]=1;
+//                     count++;
+//                     q.offer(a);
+//                 }
+//             }
+//         }
+//         return count == rooms.size();
+//     }
+// }
