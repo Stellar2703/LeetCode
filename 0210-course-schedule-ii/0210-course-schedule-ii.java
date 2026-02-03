daae8198049eba294d2e5b853521334848e2c08b
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-03 14:23:53 +0530

    Time: 6 ms (50.59%), Space: 46.9 MB (54.31%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,41 +1,36 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prereq) {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int n = numCourses;
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        int[] indeg = new int[n];
-        for (int[] edge : prereq) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(v).add(u);
-            indeg[u]++;
-        }
+        int[] indeg = new int[numCourses];
+        for(int[] edge : prerequisites){
+            int v = edge[0];
+            int u = edge[1];
+            adj.get(u).add(v);
+            indeg[v]++;
+            }
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indeg[i] == 0) {
+        for(int i=0;i<numCourses;i++){
+            if (indeg[i]==0){
                 q.offer(i);
             }
         }
-
         ArrayList<Integer> ans = new ArrayList<>();
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int node = q.poll();
             ans.add(node);
-            for (int a : adj.get(node)) {
+            for(int a : adj.get(node) ){
                 indeg[a]--;
-                if (indeg[a] == 0) {
-                    q.offer(a);
-                }
+                if(indeg[a]==0) q.offer(a);
             }
         }
-        if (ans.size() != n)
-            return new int[0];
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] result = new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
             result[i] = ans.get(i);
         }
+        if (ans.size() != numCourses) return new int[0];
         return result;
     }
 }
