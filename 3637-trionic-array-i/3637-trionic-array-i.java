8e97e4119ff8458249fc9fb28cd91540f5e4b364
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-04 16:14:22 +0530

    Time: 1 ms (71.97%), Space: 44.7 MB (65.69%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -2,27 +2,28 @@ class Solution {
     public boolean isTrionic(int[] nums) {
         int inc = 0;
         int dec = 0;
-        int index =1;
-        while (index<nums.length ){
-            if(nums[index-1]<nums[index] ){
-                while (index<nums.length &&  nums[index-1]<nums[index]) index++;
-                if(index<nums.length && nums[index-1]>nums[index]){
-                    while(index<nums.length && nums[index-1]>nums[index]) index++;
+        int index = 1;
+        while (index < nums.length) {
+            if (nums[index - 1] < nums[index]) {
+                while (index < nums.length && nums[index - 1] < nums[index])
+                    index++;
+                if (index < nums.length && nums[index - 1] > nums[index]) {
+                    while (index < nums.length && nums[index - 1] > nums[index])
+                        index++;
 
-                    if(index<nums.length &&  nums[index-1]<nums[index]){
-                    while(index<nums.length && nums[index-1]<nums[index]) index++;}
-                    else{
+                    if (index < nums.length && nums[index - 1] < nums[index]) {
+                        while (index < nums.length && nums[index - 1] < nums[index])
+                            index++;
+                    } else {
                         return false;
                     }
-                }
-                else{
+                } else {
                     return false;
                 }
-            }
-            else{
+            } 
+            else {
                 return false;
             }
-            index++;
         }
         return true;
     }
