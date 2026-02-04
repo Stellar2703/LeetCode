194010f8db58cd2f92f8998a95e0b305c4fd4646
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-04 16:17:08 +0530

    Time: 1 ms (71.97%), Space: 44.8 MB (31.38%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,29 +1,61 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
         int n = nums.length;
-        int i = 1;
-        while (i < n) {
-            if (nums[i - 1] < nums[i]) {
-                while (i < n && nums[i - 1] < nums[i])
-                    i++;
-                if (i < n && nums[i - 1] > nums[i]) {
-                    while (i < n && nums[i - 1] > nums[i])
-                        i++;
+        if (n < 3) return false;
 
-                    if (i < n && nums[i - 1] < nums[i]) {
-                        while (i < n && nums[i - 1] < nums[i])
-                            i++;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-            } 
-            else {
-                return false;
-            }
+        int i = 1;
+
+        // 1️⃣ strictly increasing
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
         }
-        return true;
+        if (i == 1 || i == n) return false; // no increase or ended too early
+
+        // 2️⃣ strictly decreasing
+        int decStart = i;
+        while (i < n && nums[i] < nums[i - 1]) {
+            i++;
+        }
+        if (i == decStart || i == n) return false; // no decrease or no third phase
+
+        // 3️⃣ strictly increasing again
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
+        }
+
+        return i == n; // must consume entire array
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean isTrionic(int[] nums) {
+//         int n = nums.length;
+//         int i = 1;
+//         while (i < n) {
+//             if (nums[i - 1] < nums[i]) {
+//                 while (i < n && nums[i - 1] < nums[i])
+//                     i++;
+//                 if (i < n && nums[i - 1] > nums[i]) {
+//                     while (i < n && nums[i - 1] > nums[i])
+//                         i++;
+
+//                     if (i < n && nums[i - 1] < nums[i]) {
+//                         while (i < n && nums[i - 1] < nums[i])
+//                             i++;
+//                     } else {
+//                         return false;
+//                     }
+//                 } else {
+//                     return false;
+//                 }
+//             } 
+//             else {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
