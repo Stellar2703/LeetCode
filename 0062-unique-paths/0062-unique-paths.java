67b35a7e642befc4600a709f5a6834e1dd0e2b96
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-05 11:12:32 +0530

    Time: 0 ms (100%), Space: 42.2 MB (61.69%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int f (int i,int j ,int m,int n ,int[][]dp){
+        if(i==m-1 && j==n-1) return 1;
+        if(i==m || j==n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        dp[i][j] = f(i, j + 1, m, n, dp) + f(i + 1, j, m, n, dp);
+    return dp[i][j];
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+        dp[i][j] = -1;
+    }
+}
+        return f(0, 0, m, n,dp);
+
+    }
+}
+
+
+// class Solution {
+//     public int f(int i, int j, int m, int n) {
+//         if (i == m - 1 && j == n - 1) return 1;
+//         if (i >= m || j >= n) return 0;
+
+//         int right = f(i, j + 1, m, n);
+//         int down  = f(i + 1, j, m, n);
+//         return right + down;
+//     }
+
+//     public int uniquePaths(int m, int n) {
+//         return f(0, 0, m, n);
+//     }
+// }
