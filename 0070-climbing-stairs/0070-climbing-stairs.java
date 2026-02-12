e24966b7f9620aedcf7bc47d7e3b04a4957ba3a2
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-12 10:06:37 +0530

    Time: 0 ms (100%), Space: 42 MB (86.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        if(n<=1) return 1;
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr=0;
+        for(int i=2;i<n+1;i++){
+            curr = prev1+prev2;
+            prev1 = prev2;
+            prev2= curr;
         }
-        return dp[n];
+        return prev2;
     }
 }
+
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         int [] dp = new int[n+1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for(int i=2;i<n+1;i++){
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
 
 
 
@@ -17,34 +33,15 @@ class Solution {
 // class Solution {
 //     public int f(int n,int[] dp){
 //         if(n==0) return 1;
-//         if(n<=0) return 0;
+//         if(n<0) return 0;
 //         if(dp[n]!=-1) return dp[n];
-//         int one = f(n-1,dp);
-//         int two = f(n-2,dp);
-//         dp[n] = one + two;
-//         return dp[n];
+//         int pick = f(n-1,dp);
+//         int not_pick = f(n-2,dp);
+//         return dp[n] = pick+not_pick;
 //     }
 //     public int climbStairs(int n) {
 //         int[] dp = new int[n+1];
 //         Arrays.fill(dp,-1);
 //         return f(n,dp);
-//     }
-// }
-
-
-
-
-
-
-//    ??? ----  RECURSION ---- ???
-// class Solution {
-//     public int f(int n){
-//         if(n<=1) return 1;
-//         int pick = f(n-1);
-//         int notpick  =f(n-2);
-//         return pick+notpick;
-//     }
-//     public int climbStairs(int n) {
-//         return f(n);
 //     }
 // }
