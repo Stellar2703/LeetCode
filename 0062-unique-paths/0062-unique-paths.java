44191de8c286be7a04959c26e8ebbc3db145c52a
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-13 16:00:29 +0530

    Time: 1 ms (14.65%), Space: 42.4 MB (26.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,44 @@
 class Solution {
-    public int f (int i,int j ,int m,int n ,int[][]dp){
-        if(i==m-1 && j==n-1) return 1;
-        if(i==m || j==n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j] = f(i, j + 1, m, n, dp) + f(i + 1, j, m, n, dp);
-    return dp[i][j];
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-        dp[i][j] = -1;
-    }
-}
-        return f(0, 0, m, n,dp);
+        int[][] dp = new int[n][m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int right = (i + 1 < n) ? dp[i+1][j] : 0;
+                    int down  = (j + 1 < m) ? dp[i][j+1] : 0;
 
+                    dp[i][j] = right + down;
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
+
+
+
+// class Solution {
+//     public int f (int i,int j ,int m,int n ,int[][]dp){
+//         if(i==m-1 && j==n-1) return 1;
+//         if(i==m || j==n) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         dp[i][j] = f(i, j + 1, m, n, dp) + f(i + 1, j, m, n, dp);
+//     return dp[i][j];
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for (int i = 0; i < m; i++) {
+//     for (int j = 0; j < n; j++) {
+//         dp[i][j] = -1;
+//     }
+// }
+//         return f(0, 0, m, n,dp);
+
+//     }
+// }
 
 
 // class Solution {
