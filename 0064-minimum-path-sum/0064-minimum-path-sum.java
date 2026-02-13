f5ff266130b48fc87920116ad2987ba99448532d
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-13 15:50:27 +0530

    Time: 7 ms (10.56%), Space: 50.2 MB (70.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,21 +1,41 @@
 class Solution {
-    public int f(int [][] grid , int i,int j,int[][] dp){
-        if(i==grid.length-1 && j== grid[0].length-1) return grid[i][j] ;
-        if(i>=grid.length || j>= grid[0].length) return 1000000;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int right = grid[i][j] + f(grid,i,j+1,dp);
-        int down = grid[i][j] + f(grid,i+1,j,dp);
-
-        return dp[i][j]= Math.min(right,down);
-
-    }
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int [][] dp = new int [n+1][m+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[n][m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1)dp[i][j] = grid[i][j];
+                else{
+                    int right = j+1<m ? dp[i][j+1] : 1000000;
+                    int down = i+1<n ? dp[i+1][j] : 1000000;
+                    dp[i][j] = grid[i][j] + Math.min(right,down);
+                }
+            }
         }
-        return f(grid,0,0,dp);
+            return dp[0][0];
     }
 }
+
+
+// // class Solution {
+//     public int f(int [][] grid , int i,int j,int[][] dp){
+//         if(i==grid.length-1 && j== grid[0].length-1) return grid[i][j] ;
+//         if(i>=grid.length || j>= grid[0].length) return 1000000;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int right = grid[i][j] + f(grid,i,j+1,dp);
+//         int down = grid[i][j] + f(grid,i+1,j,dp);
+
+//         return dp[i][j]= Math.min(right,down);   
+
+//     }
+//     public int minPathSum(int[][] grid) {
+//         int n = grid.length;
+//         int m = grid[0].length;
+//         int [][] dp = new int [n+1][m+1];
+//         for(int i=0;i<=n;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         return f(grid,0,0,dp);
+//     }
+// }
