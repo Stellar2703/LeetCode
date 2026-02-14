836dd2450d0e9881d6cc21022968422257bcf6b6
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-14 13:58:39 +0530

    Time: 8 ms (31.65%), Space: 47.1 MB (90.28%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int f(int[][] matrix, int i,int j,Integer[][]dp){
-        int n = matrix.length;
-        if(i==n-1) return matrix[i][j];
-        if(dp[i][j]!= null) return dp[i][j];
-        int left = Integer.MAX_VALUE, middle = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
-        if(j>0 && j<n) left = matrix[i][j] + f(matrix,i+1,j-1,dp);
-        if(j>=0 && j<n) middle = matrix[i][j] + f(matrix,i+1,j,dp);
-        if(j>=0 && j<n-1) right = matrix[i][j] + f(matrix,i+1,j+1,dp);
-        return dp[i][j] = Math.min(left,Math.min(right,middle));
-    }
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
+        int [][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            dp[n-1][i] = matrix[n-1][i];
+        }
+        for(int i = n-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                int left = j-1>=0 ? matrix[i][j] + dp[i+1][j-1] : Integer.MAX_VALUE;
+                int middle = matrix[i][j] + dp[i+1][j];
+                int right = j+1<n ? matrix[i][j] + dp[i+1][j+1] : Integer.MAX_VALUE;
+
+                dp[i][j] = Math.min(left,Math.min(middle,right));
+            }
+        }
         int path = Integer.MAX_VALUE;
-        Integer[][] dp = new Integer [n][n];
-        for(int i = 0;i<n;i++){
-            path = Math.min(path,f(matrix,0,i,dp));
+        for (int j = 0; j < n; j++) {
+            path = Math.min(path, dp[0][j]);
         }
         return path;
     }
@@ -23,10 +25,27 @@ class Solution {
 
 
 
-
-
-
-
+// class Solution {
+//     public int f(int[][] matrix, int i,int j,Integer[][]dp){
+//         int n = matrix.length;
+//         if(i==n-1) return matrix[i][j];
+//         if(dp[i][j]!= null) return dp[i][j];
+//         int left = Integer.MAX_VALUE, middle = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
+//         if(j>0 && j<n) left = matrix[i][j] + f(matrix,i+1,j-1,dp);
+//         if(j>=0 && j<n) middle = matrix[i][j] + f(matrix,i+1,j,dp);
+//         if(j>=0 && j<n-1) right = matrix[i][j] + f(matrix,i+1,j+1,dp);
+//         return dp[i][j] = Math.min(left,Math.min(right,middle));
+//     }
+//     public int minFallingPathSum(int[][] matrix) {
+//         int n = matrix.length;
+//         int path = Integer.MAX_VALUE;
+//         Integer[][] dp = new Integer [n][n];
+//         for(int i = 0;i<n;i++){
+//             path = Math.min(path,f(matrix,0,i,dp));
+//         }
+//         return path;
+//     }
+// }
 
 
 
