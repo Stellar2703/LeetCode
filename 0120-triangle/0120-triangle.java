cacdf3d01156021bc12d9f6edccdd88f198ed29e
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-14 10:09:05 +0530

    Time: 3 ms (82.07%), Space: 45.6 MB (64.48%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,44 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int m = triangle.get(n-1).size();
-        int[][] dp = new int[n][m];
-        for(int i=0;i<m;i++){
-            dp[n-1][i] = triangle.get(n-1).get(i);
+        int[] dp = new int[n];
+        for(int i=0;i<n;i++){
+            dp[i] = triangle.get(n-1).get(i);
         }
         for(int i = n-2;i>=0;i--){
-            for(int j = 0 ;j<=i;j++){
-                int right = triangle.get(i).get(j) + dp[i+1][j+1];
-                int left = triangle.get(i).get(j)+dp[i+1][j];
-                dp[i][j] = Math.min(right,left);
+            for(int j=0;j<=i;j++){
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j+1]);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         int m = triangle.get(n-1).size();
+//         int[][] dp = new int[n][m];
+//         for(int i=0;i<m;i++){
+//             dp[n-1][i] = triangle.get(n-1).get(i);
+//         }
+//         for(int i = n-2;i>=0;i--){
+//             for(int j = 0 ;j<=i;j++){
+//                 int right = triangle.get(i).get(j) + dp[i+1][j+1];
+//                 int left = triangle.get(i).get(j)+dp[i+1][j];
+//                 dp[i][j] = Math.min(right,left);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
 
 
 // class Solution {
