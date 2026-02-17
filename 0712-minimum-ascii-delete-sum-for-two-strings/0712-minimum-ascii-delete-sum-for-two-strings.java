2ccb7d047e9446e5f144bdc72a198d6ea38d39cb
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-17 09:51:16 +0530

    Time: 20 ms (46.17%), Space: 46.5 MB (50.85%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -2,33 +2,57 @@ class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int [n+1][m+1];
-        int sum =0;
-        for(int i=1;i<=n;i++){
-            sum +=(int)s1.charAt(i-1);
-            dp[i][0]=sum;
-        }
-        sum=0;
+        int[] prev =new int[m+1];
         for(int i=1;i<=m;i++){
-            sum+=(int) s2.charAt(i-1);
-            dp[0][i] = sum;
+            prev[i] = prev[i-1]+s2.charAt(i-1);
         }
         for(int i=1;i<=n;i++){
+            int [] curr = new int[m+1];
+            curr[0] = prev[0]+s1.charAt(i-1);
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                if(s1.charAt(i-1)==s2.charAt(j-1)) curr[j] = prev[j-1];
                 else{
-                    dp[i][j] = Math.min(s1.charAt(i-1)+dp[i-1][j],s2.charAt(j-1)+dp[i][j-1]);
+                    curr[j] = Math.min(s1.charAt(i-1)+prev[j],s2.charAt(j-1)+curr[j-1]);
                 }
             }
+            prev  = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
 
 
+// class Solution {
+//     public int minimumDeleteSum(String s1, String s2) {
+//         int n = s1.length();
+//         int m = s2.length();
+//         int[][] prev = new int [n+1][m+1];
+//         int sum =0;
+//         for(int i=1;i<=n;i++){
+//             sum +=(int)s1.charAt(i-1);
+//             prev[i][0]=sum;
+//         }
+//         sum=0;
+//         for(int i=1;i<=m;i++){
+//             sum+=(int) s2.charAt(i-1);
+//             prev[0][i] = sum;
+//         }
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s1.charAt(i-1)==s2.charAt(j-1)) prev[i][j] = prev[i-1][j-1];
+//                 else{
+//                     prev[i][j] = Math.min(s1.charAt(i-1)+prev[i-1][j],s2.charAt(j-1)+prev[i][j-1]);
+//                 }
+//             }
+//         }
+//         return prev[n][m];
+//     }
+// }
+
+
 
 // class Solution {
-//     int f(String s1,String s2,int i, int j,int[][] dp){
+//     int f(String s1,String s2,int i, int j,int[][] prev){
 //         if(i<0){
 //             int sum =0;
 //             for(int x = j;x>=0;x--){
@@ -43,17 +67,17 @@ class Solution {
 //             }
 //             return sum;
 //         }
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         if(s1.charAt(i)==s2.charAt(j)) return f(s1,s2,i-1,j-1,dp);
+//         if(prev[i][j]!=-1) return prev[i][j];
+//         if(s1.charAt(i)==s2.charAt(j)) return f(s1,s2,i-1,j-1,prev);
 
-//         return dp[i][j] = Math.min((int) s1.charAt(i) + f(s1,s2,i-1,j,dp),
-//                         (int) s2.charAt(j) + f(s1,s2,i,j-1,dp));
+//         return prev[i][j] = Math.min((int) s1.charAt(i) + f(s1,s2,i-1,j,prev),
+//                         (int) s2.charAt(j) + f(s1,s2,i,j-1,prev));
 //     }
 //     public int minimumDeleteSum(String s1, String s2) {
-//         int[][] dp =new int[s1.length()][s2.length()];
+//         int[][] prev =new int[s1.length()][s2.length()];
 //         for(int i=0;i<s1.length();i++){
-//             Arrays.fill(dp[i],-1);
+//             Arrays.fill(prev[i],-1);
 //         }
-//         return f(s1,s2,s1.length()-1,s2.length()-1,dp);
+//         return f(s1,s2,s1.length()-1,s2.length()-1,prev);
 //     }
 // }
