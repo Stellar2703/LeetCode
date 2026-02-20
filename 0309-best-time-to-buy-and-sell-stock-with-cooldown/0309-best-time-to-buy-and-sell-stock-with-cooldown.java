f60b5d3c82ea589803722a303acb83a561723726
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-20 09:59:35 +0530

    Time: 0 ms (100%), Space: 43.2 MB (97.46%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,25 +1,46 @@
 class Solution {
-
-    public int f(int index,int canBuy,int[] prices,Integer[][] dp){
-        if(index>=prices.length) return 0;
-        if(dp[index][canBuy]!= null) return dp[index][canBuy];
-
-        if(canBuy==1){
-            return dp[index][canBuy] = Math.max(
-                -prices[index]+f(index+1,0,prices,dp),
-                f(index+1,1,prices,dp)
-            );
-        }
-        else{
-            return dp[index][canBuy] = Math.max(
-                prices[index]+f(index+2,1,prices,dp),
-                f(index+1,0,prices,dp)
-            );
-        }
-
-    }
     public int maxProfit(int[] prices) {
-        Integer [][] dp = new Integer [prices.length][2];
-        return f(0,1,prices,dp);   
+        int n = prices.length;
+        int[][] dp = new int[n + 2][2];
+        for (int index = n - 1; index >= 0; index--) {
+            for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                if (canBuy == 1) {
+                    dp[index][canBuy] = Math.max(
+                            -prices[index] + dp[index + 1][0],
+                            dp[index + 1][1]);
+                } else {
+                    dp[index][canBuy] = Math.max(
+                            prices[index] + dp[index + 2][1],
+                            dp[index + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 }
+
+// class Solution {
+
+//     public int f(int index,int canBuy,int[] prices,Integer[][] dp){
+//         if(index>=prices.length) return 0;
+//         if(dp[index][canBuy]!= null) return dp[index][canBuy];
+
+//         if(canBuy==1){
+//             return dp[index][canBuy] = Math.max(
+//                 -prices[index]+f(index+1,0,prices,dp),
+//                 f(index+1,1,prices,dp)
+//             );
+//         }
+//         else{
+//             return dp[index][canBuy] = Math.max(
+//                 prices[index]+f(index+2,1,prices,dp),
+//                 f(index+1,0,prices,dp)
+//             );
+//         }
+
+//     }
+//     public int maxProfit(int[] prices) {
+//         Integer [][] dp = new Integer [prices.length][2];
+//         return f(0,1,prices,dp);   
+//     }
+// }
