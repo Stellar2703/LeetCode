5471ff2d453b97028f94cceb7369f6578aa47c8a
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-20 15:17:06 +0530

    Time: 20 ms (34.99%), Space: 98 MB (65.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void f(int n , int k ,int start,List<List<Integer>> ans,List<Integer> temp ){
+        if(k==0){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i=start;i<=n;i++){
+            temp.add(i);
+            f(n,k-1,i+1,ans,temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        f(n,k,1,ans,temp);
+        return ans; 
+    }
+}
