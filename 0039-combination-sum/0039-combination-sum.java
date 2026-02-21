49e4950c6f18fbf436e71af1d007ca36f4b8b950
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-21 13:46:19 +0530

    Time: 3 ms (36.8%), Space: 46.1 MB (12.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,55 @@
-class Solution {
-    private void f(int index, int[] nums, int target, int sum, List<List<Integer>> ans,
-            List<Integer> temp) {
-        if (sum == target)
-            ans.add(new ArrayList<>(temp));
-        if (index == nums.length)
-            return;
-        if(sum>target) return;
+// class Solution {
+//     private void f(int index, int[] nums, int target, int sum, List<List<Integer>> ans,
+//             List<Integer> temp) {
+//         if (sum == target)
+//             ans.add(new ArrayList<>(temp));
+//         if (index == nums.length)
+//             return;
+//         if(sum>target) return;
 
-        for (int i = index; i < nums.length; i++) {
-            sum += nums[i];
-            temp.add(nums[i]);
-            f(i, nums, target, sum, ans, temp);
-            sum -= nums[i];
-               temp.remove(temp.size() - 1);
+//         for (int i = index; i < nums.length; i++) {
+//             sum += nums[i];
+//             temp.add(nums[i]);
+//             f(i, nums, target, sum, ans, temp);
+//             sum -= nums[i];
+//             temp.remove(temp.size() - 1);
             
+//         }
+
+//     }
+
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         List<Integer> temp = new ArrayList<>();
+//         f(0, candidates, target, 0, ans, temp);
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    private void f(int start, int[] nums, int target,
+                   List<List<Integer>> ans, List<Integer> temp) {
+
+        if (target == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
         }
 
+        for (int i = start; i < nums.length; i++) {
+
+            if (nums[i] > target) break;   // 🔥 pruning
+
+            temp.add(nums[i]);
+            f(i, nums, target - nums[i], ans, temp);
+            temp.remove(temp.size() - 1);
+        }
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);  // 🔥 important
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        f(0, candidates, target, 0, ans, temp);
+        f(0, candidates, target, ans, new ArrayList<>());
         return ans;
     }
 }
