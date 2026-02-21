1feac072a61612c7de4de973f7136c33a2fbf00a
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-21 14:08:44 +0530

    Time: 6 ms (73.3%), Space: 45.6 MB (19.51%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,28 +1,30 @@
+import java.util.*;
+
 class Solution {
-    private void f(int index, int[] nums, int target, int sum, HashSet<List<Integer>> ans,
-            List<Integer> temp) {
-        if (sum == target)
+    private void f(int index, int[] nums, int target, int sum, HashSet<List<Integer>> ans, List<Integer> temp) {
+        if (sum == target) {
             ans.add(new ArrayList<>(temp));
-        if (index == nums.length)
             return;
-        if(sum>target) return;
+        }
+        if (sum > target) return;
 
         for (int i = index; i < nums.length; i++) {
+            // Skip duplicates
             if (i > index && nums[i] == nums[i - 1]) continue;
-            sum += nums[i];
+
+            if (sum + nums[i] > target) break; // Early pruning
+
             temp.add(nums[i]);
-            f(i+1, nums, target, sum, ans, temp);
-            sum -= nums[i];
-            temp.remove(temp.size() - 1); 
+            f(i + 1, nums, target, sum + nums[i], ans, temp);
+            temp.remove(temp.size() - 1);
         }
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);  // Sort to handle duplicates
         HashSet<List<Integer>> ans = new HashSet<>();
-         Arrays.sort(candidates);
         List<Integer> temp = new ArrayList<>();
         f(0, candidates, target, 0, ans, temp);
         return new ArrayList<>(ans);
     }
 }
-
