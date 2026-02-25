038dc11fe47d61b984183792b06e6cd9b1b32cce
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-02-25 15:09:55 +0530

    Time: 0 ms (100%), Space: 46.3 MB (56.7%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -23,19 +23,54 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root==null) return root;
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                Node node = q.poll();
-                if(i == size-1) node.next = null;
-                else node.next = q.peek();
-                if(node.left!=null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
+        if (root == null) return null;
+
+        if (root.left != null) {
+            if (root.right != null) {
+                root.left.next = root.right;
+            } else {
+                root.left.next = getNext(root.next);
             }
         }
+
+        if (root.right != null) {
+            root.right.next = getNext(root.next);
+        }
+
+        // IMPORTANT: process right first
+        connect(root.right);
+        connect(root.left);
+
         return root;
     }
+
+    private Node getNext(Node node) {
+        while (node != null) {
+            if (node.left != null) return node.left;
+            if (node.right != null) return node.right;
+            node = node.next;
+        }
+        return null;
+    }
 }
+
+
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null) return root;
+//         Queue<Node> q = new LinkedList<>();
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i=0;i<size;i++){
+//                 Node node = q.poll();
+//                 if(i == size-1) node.next = null;
+//                 else node.next = q.peek();
+//                 if(node.left!=null) q.offer(node.left);
+//                 if(node.right != null) q.offer(node.right);
+//             }
+//         }
+//         return root;
+//     }
+// }
