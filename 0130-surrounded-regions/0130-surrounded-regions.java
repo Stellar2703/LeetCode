511a70f8c55eadaa2024d0159da8a3e63f48f8dd
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-03-10 18:13:38 +0530

    Time: 4 ms (18.23%), Space: 47.8 MB (54.26%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,74 +1,59 @@
-class Pair {
-    int row;
-    int col;
-    Pair(int _row, int _col) {
-        this.row = _row;
-        this.col = _col;
-    }
-}
-
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-
-        int[][] vis = new int[n][m];
+        int[][] vis = new int [n][m];
         Queue<Pair> q = new LinkedList<>();
-
-        // Top & Bottom row
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') {
-                q.offer(new Pair(0, j));
-                vis[0][j] = 1;
-            }
-            if (board[n - 1][j] == 'O') {
-                q.offer(new Pair(n - 1, j));
-                vis[n - 1][j] = 1;
-            }
-        }
-
-        // Left & Right column
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') {
-                q.offer(new Pair(i, 0));
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                q.offer(new Pair(i,0));
                 vis[i][0] = 1;
             }
-            if (board[i][m - 1] == 'O') {
-                q.offer(new Pair(i, m - 1));
-                vis[i][m - 1] = 1;
+            if(board[i][m-1]=='O'){
+                q.offer(new Pair(i,m-1));
+                vis[i][m-1] = 1 ;
             }
         }
-
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-
-        // BFS
-        while (!q.isEmpty()) {
-            Pair cur = q.poll();
-            int row = cur.row;
-            int col = cur.col;
-
-            board[row][col] = 'T'; // mark safe
-
-            for (int k = 0; k < 4; k++) {
-                int nrow = row + drow[k];
-                int ncol = col + dcol[k];
-
-                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
-                    board[nrow][ncol] == 'O' && vis[nrow][ncol] == 0) {
-
+        for(int j = 0;j<m;j++){
+            if(board[0][j] == 'O'){
+                q.offer(new Pair(0,j));
+                vis[0][j] =1 ;
+            }
+            if(board[n-1][j]=='O'){
+                q.offer(new Pair(n-1,j));
+                vis[n-1][j] = 1;
+            }
+        }
+        int[] drow = {0,1,0,-1};
+        int[] dcol = {1,0,-1,0};
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.poll();
+            for(int i=0;i<4;i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && board[nrow][ncol]=='O' && vis[nrow][ncol]!=1){
                     vis[nrow][ncol] = 1;
-                    q.offer(new Pair(nrow, ncol));
+                    q.offer(new Pair(nrow,ncol));
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
 
-        // Final conversion
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == 'T') board[i][j] = 'O';
-            }
-        }
     }
 }
