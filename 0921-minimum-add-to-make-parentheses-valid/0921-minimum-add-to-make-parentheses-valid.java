fb8782abfb196446523728e770fb3cf7c3ddf644
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-03-11 14:06:29 +0530

    Time: 0 ms (100%), Space: 42.8 MB (86.86%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,19 +1,39 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c==')'){
-                if(!st.isEmpty() && st.peek()=='('){
-                    st.pop();
-                }
-                else{
-                    st.push(c);
-                }
+//     public int minAddToMakeValid(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(char c : s.t    oCharArray()){
+//             if(c==')'){
+//                 if(!st.isEmpty() && st.peek()=='('){
+//                     st.pop();
+//                 }
+//                 else{
+//                     st.push(c);
+//                 }
+//             }
+//             else{
+//                     st.push(c);
+//                 }
+//         }
+//         return st.size();
+//     }
+// }
+
+
+public int minAddToMakeValid(String s) {
+    int open = 0; // count of unmatched '('
+    int close = 0; // count of unmatched ')'
+
+    for(char c : s.toCharArray()){
+        if(c=='('){
+            open++;
+        } else {
+            if(open>0){
+                open--; // match with '('
+            } else {
+                close++; // unmatched ')'
             }
-            else{
-                    st.push(c);
-                }
         }
-        return st.size();
     }
+    return open + close;
+}
 }
