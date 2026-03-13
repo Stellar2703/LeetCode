5129e7392eda46b5b79ee98662eadbf8eae08236
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-03-13 10:54:04 +0530

    Time: 1 ms (87.17%), Space: 47.4 MB (37.05%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,25 +1,47 @@
+// class Solution {
+//     public int findUnsortedSubarray(int[] nums) {
+//         Stack<Integer> stack = new Stack<>();
+//         int st = nums.length, end = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+//                 st = Math.min(st, stack.pop());
+//             }
+//             stack.push(i);
+//         }
+
+//         stack.clear();
+
+//         for (int i = nums.length - 1; i >= 0; i--) {
+//             while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+//                 end = Math.max(end, stack.pop());
+//             }
+//             stack.push(i);
+//         }
+
+//         if (end <= st) return 0;
+//         return end - st + 1;
+//     }
+// }
+
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Stack<Integer> stack = new Stack<>();
-        int st = nums.length, end = 0;
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
-                st = Math.min(st, stack.pop());
-            }
-            stack.push(i);
+        int max = nums[0];
+        int min = nums[n-1];
+        int start = -1, end = -2;
+
+        for(int i = 1; i < n; i++){
+            max = Math.max(max, nums[i]);
+            if(nums[i] < max) end = i;
         }
 
-        stack.clear();
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-                end = Math.max(end, stack.pop());
-            }
-            stack.push(i);
+        for(int i = n-2; i >= 0; i--){
+            min = Math.min(min, nums[i]);
+            if(nums[i] > min) start = i;
         }
 
-        if (end <= st) return 0;
-        return end - st + 1;
+        return end - start + 1;
     }
 }
