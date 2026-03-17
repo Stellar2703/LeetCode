b1d7bb258f49eaae9d6ba3f364e0d30714fdac89
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-03-17 18:07:29 +0530

    Time: 12 ms (47.03%), Space: 46.1 MB (88.17%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,40 +1,16 @@
-// class Solution {
-//     public int characterReplacement(String s, int k) {
-//         HashMap<Character,Integer> mp = new HashMap<>();
-//         int l=0,freq=0,ans = 0;
-//         for(int r=0;r<s.length();r++){
-//             char right = s.charAt(r);
-//             mp.put(right,mp.getOrDefault(right,0)+1);
-//             freq = Math.max(freq,mp.get(right));
-//             if(r-l+1-freq<=k){
-//                 ans = Math.max(r-l+1,ans);
-//             }
-//             char left = s.charAt(l);
-//             if(r-l+1-freq>k){
-//                 mp.put(left,mp.get(left)-1);
-//                 l++;
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] count = new int[26];
-        int l = 0, maxFreq = 0, ans = 0;
-
-        for (int r = 0; r < s.length(); r++) {
-            int idx = s.charAt(r) - 'A';
-            count[idx]++;
-            maxFreq = Math.max(maxFreq, count[idx]);
-
-            if (r - l + 1 - maxFreq > k) {
-                count[s.charAt(l) - 'A']--;
+        int[] arr = new int[26];
+        int freq = 0,ans = 0,l=0;
+        for(int r =0;r<s.length();r++){
+            arr[s.charAt(r)-'A']+=1;
+            freq = Math.max(arr[s.charAt(r)-'A'],freq);
+            if((r-l+1-freq)>k ){
+                arr[s.charAt(l)-'A']-=1;
                 l++;
             }
-
-            ans = Math.max(ans, r - l + 1);
+            ans = Math.max(ans,r-l+1);
+            // System.out.println(l+" "+r);
         }
         return ans;
     }
