1cbe9642beedeb40dda4a7099c1f71d2a4050761
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-04-03 11:30:49 +0530

    Time: 1 ms (85.64%), Space: 43.3 MB (45.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,38 +1,52 @@
 class Solution {
-    public int f(String s,int index,int[] dp){
-        if(index==s.length()) return 1;
-        if(s.charAt(index)=='0') return 0;
-        if(dp[index]!=-1) return dp[index];
-        int pick = f(s,index+1,dp);
-        int picktwo=0;
-        if(index+1<s.length() && Integer.parseInt(s.substring(index,index+2))<=26){
-            picktwo = f(s,index+2,dp);
-        }
-        dp[index] = pick + picktwo;
-        return dp[index];
-    }
     public int numDecodings(String s) {
-        int [] dp =  new int[s.length()];
-        Arrays.fill(dp,-1);
-        return f(s,0,dp);
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[n] = 1;
+        int oneDigit = 0;
+        int twoDigit = 0;
+        for(int i=n-1;i>=0;i--){
+            oneDigit = 0;
+            twoDigit = 0;
+            if(s.charAt(i)!='0'){
+                oneDigit = dp[i+1];
+                if(i<n-1 && Integer.parseInt(s.substring(i,i+2))<=26){
+                    twoDigit = dp[i+2];
+                }
+            }
+            dp[i] = oneDigit + twoDigit;
+        }
+        return dp[0];
     }
 }
 
 
 
+
 // class Solution {
-//     public int f(String s,int index){
-//         if(index==s.length()) return 1;
-//         if(s.charAt(index)=='0') return 0;
-//         int pick = f(s,index+1);
-//         int picktwo = 0;
-//         if(index+1<s.length() && Integer.parseInt(s.substring(index,index+2))<=26){
-//             picktwo = f(s,index+2);
+//     public int f(int index, String s,int [] dp) {
+//         if (index >= s.length())
+//             return 1;
+//         if (s.charAt(index) == '0')
+//             return 0;
+
+//         if(dp[index]!=-1) return dp[index];
+//         int oneDigit = f(index + 1, s,dp);
+
+//         int twoDigit = 0;
+//         if (index < s.length() - 1) {
+//             int num = (s.charAt(index) - '0') * 10 + (s.charAt(index + 1) - '0');
+//             if (num <= 26) {
+//                 twoDigit = f(index + 2, s,dp);
+//             }
 //         }
 
-//         return pick+picktwo;
+//         return dp[index] = oneDigit + twoDigit;
 //     }
+
 //     public int numDecodings(String s) {
-//         return f(s,0);
+//         int [] dp =  new int[s.length()];
+//         Arrays.fill(dp,-1);
+//         return f(0, s,dp); 
 //     }
 // }
