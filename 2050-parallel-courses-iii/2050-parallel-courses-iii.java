b8097f2b179770647e63077c654f017f2802b628
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-04-28 11:05:58 +0530

    Time: 19 ms (89.59%), Space: 109.2 MB (90.83%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,30 +1,38 @@
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        int[] indeg = new int[n+1];
-        for(int i=0;i<n+1;i++){
+        for(int i =0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<relations.length;i++){
-            adj.get(relations[i][0]).add(relations[i][1]);
-            indeg[relations[i][1]]++;
+        int[] indeg= new int[n+1];
+        int[] dp = new int[n+1];
+        Queue<Integer> q = new LinkedList<>();
+        for(int [] edge : relations){
+            int u =edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            indeg[v]++;
         }
-        int [] dp =  new int[n+1];
-        for(int i =1;i<n+1;i++){
-            if(indeg[i]==0) q.offer(i);
-            dp[i] = time[i-1];
+        for(int i =1;i<=n;i++){
+            if(indeg[i]==0) {
+                q.offer(i);
+                dp[i] = time[i-1];
+            }
         }
-        int ans = 0;
+
         while(!q.isEmpty()){
-            int size = q.size();
-                int node = q.poll();
-                ans = Math.max(ans,dp[node]);
-                for(int j : adj.get(node)){
-                    indeg[j]--;
-                    dp[j] = Math.max(dp[j],dp[node]+time[j-1]);
-                    if(indeg[j]==0) q.offer(j);
+            int node = q.poll();
+            for(int edge : adj.get(node)){
+                indeg[edge]--;
+                dp[edge] = Math.max(dp[edge],dp[node]+time[edge-1]);
+                if(indeg[edge]==0){
+                    q.offer(edge);
                 }
+            }
+        }
+        int ans =0;
+        for(int i =1;i<=n;i++){
+            ans = Math.max(dp[i],ans);
         }
         return ans;
     }
