7d42adb0e323d7fce78878b7602310af37e4af92
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-05-20 23:40:42 +0530

    Time: 2 ms (70.46%), Space: 47.3 MB (51.46%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -2,19 +2,38 @@ class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         int n = A.length;
         int [] ans = new int[n];
+        int []  freq = new int[n];
+        int count =0;
         for(int i=0;i<n;i++){
-            int count =0;
-            HashSet<Integer> temp = new HashSet<>();
-            for(int j=0;j<=i;j++){
-                temp.add(A[j]);
-            }
-            for(int j=0;j<=i;j++){
-                if(temp.contains(B[j])){
-                    count++;
-                }
-            }
+            freq[A[i]-1]++;
+            if(freq[A[i]-1]==2) count++;
+            freq[B[i]-1]++;
+            if(freq[B[i]-1]==2) count++;
             ans[i] = count;
         }
         return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+//         int n = A.length;
+//         int [] ans = new int[n];
+//         for(int i=0;i<n;i++){
+//             int count =0;
+//             HashSet<Integer> temp = new HashSet<>();
+//             for(int j=0;j<=i;j++){
+//                 temp.add(A[j]);
+//             }
+//             for(int j=0;j<=i;j++){
+//                 if(temp.contains(B[j])){
+//                     count++;
+//                 }
+//             }
+//             ans[i] = count;
+//         }
+//         return ans;
+//     }
+// }
