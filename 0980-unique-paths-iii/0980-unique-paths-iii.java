95ababe188465586e05e740bbada730e2319b7c0
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-05-30 20:51:24 +0530

    Time: 1 ms (39.8%), Space: 44.1 MB (12.88%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,51 +1,76 @@
 class Solution {
-    int end_row;
-    int end_col;
     int vis_count;
-    int[][] vis;
-    int ans  =0;
-    private void f(int row,int col,int[][] grid,int count){
-        if(end_row==row && end_col==col && vis_count==count){
-            ans++;
+    int ans = 0;
+
+    // private void f(int row, int col, int[][] grid, int count) {
+    //     if (grid[row][col] == 2 && count == 0) {
+    //         ans++;
+    //         return;
+    //     }
+
+    //     int[] drow = { 0, 1, 0, -1 };
+    //     int[] dcol = { -1, 0, 1, 0 };
+    //     for (int i = 0; i < 4; i++) {
+    //         int nrow = row + drow[i];
+    //         int ncol = col + dcol[i];
+    //         if (nrow >= 0 && ncol >= 0 && nrow < grid.length && ncol < grid[0].length && grid[nrow][ncol] != -1
+    //                 && grid[nrow][ncol] != -2) {
+    //             int temp = grid[nrow][ncol];
+    //             grid[nrow][ncol] = -2;
+    //             f(nrow, ncol, grid, count - 1);
+    //             grid[nrow][ncol] = temp;
+    //         }
+    //     }
+    // }
+
+    private void f(int row, int col, int[][] grid, int count) {
+
+        if (grid[row][col] == 2) {
+            if (count == 0)
+                ans++;
             return;
         }
 
-        int[] drow = {0,1,0,-1};
-        int[] dcol = {-1,0,1,0};
-        for(int i=0;i<4;i++){
-            int nrow  = row+drow[i];
-            int ncol = col+dcol[i];
-            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && grid[nrow][ncol]!= -1 && vis[nrow][ncol]!=1){
-                vis[nrow][ncol]=1;
-                f(nrow,ncol,grid,count+1);
-                vis[nrow][ncol]=0;
+        int temp = grid[row][col];
+        grid[row][col] = -2; // mark CURRENT cell
+
+        int[] drow = { 0, 1, 0, -1 };
+        int[] dcol = { -1, 0, 1, 0 };
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+
+            if (nrow >= 0 && ncol >= 0 &&
+                    nrow < grid.length && ncol < grid[0].length &&
+                    grid[nrow][ncol] != -1 && grid[nrow][ncol] != -2) {
+
+                f(nrow, ncol, grid, count - 1);
             }
         }
+
+        grid[row][col] = temp; // unmark
     }
+
     public int uniquePathsIII(int[][] grid) {
-        int n  = grid.length;
+        int n = grid.length;
         int m = grid[0].length;
         int start_row = 0;
         int start_col = 0;
-        vis = new int[n][m];
-        for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
                     start_row = i;
                     start_col = j;
                 }
-                if(grid[i][j]==2){
-                    end_row = i;
-                    end_col = j;
-                }
-                if(grid[i][j]!=-1){
+                if (grid[i][j] != -1) {
                     vis_count++;
                 }
-                
+
             }
         }
-        vis[start_row][start_col]=1;
-        f(start_row,start_col,grid,1);
+        grid[start_row][start_col] = -2;
+        f(start_row, start_col, grid, vis_count - 1);
         return ans;
     }
 }
