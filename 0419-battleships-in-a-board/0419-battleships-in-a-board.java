1dc8e34757b5aaf10f97c6a06b7fbf0dce7cbb0a
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-06-19 09:47:20 +0530

    Time: 1 ms (98.74%), Space: 45.4 MB (65.19%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,36 +1,59 @@
+// class Solution {
+//     class Pair{
+//         int row;
+//         int col;
+//         Pair(int row, int col){
+//             this.row=row;
+//             this.col =col;
+//         }
+//     }
+//     public int countBattleships(char[][] board) {
+//         Queue<Pair> q = new LinkedList<>();
+//         int n = board.length;
+//         int m = board[0].length;
+//         int[][] vis =new int[n][m];
+//         int count =0;
+//         for(int i =0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(board[i][j]=='X' && vis[i][j]!=1){
+//                     q.offer(new Pair(i,j));
+//                     count++;
+//                     while(!q.isEmpty()){
+//                         Pair p = q.poll();
+//                         if(p.row>=0 && p.row<n &&p.col>=0 && p.col<m && board[p.row][p.col]=='X' && vis[p.row][p.col]!=1){
+//                             vis[p.row][p.col]=1;
+//                             q.offer(new Pair(p.row+1,p.col));
+//                             q.offer(new Pair(p.row-1,p.col));
+//                             q.offer(new Pair(p.row,p.col+1));
+//                             q.offer(new Pair(p.row,p.col-1));
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        Pair(int row, int col){
-            this.row=row;
-            this.col =col;
-        }
-    }
     public int countBattleships(char[][] board) {
-        Queue<Pair> q = new LinkedList<>();
+        int count = 0;
         int n = board.length;
         int m = board[0].length;
-        int[][] vis =new int[n][m];
-        int count =0;
-        for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]=='X' && vis[i][j]!=1){
-                    q.offer(new Pair(i,j));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i - 1][j] == 'X') continue;
+                    if (j > 0 && board[i][j - 1] == 'X') continue;
                     count++;
-                    while(!q.isEmpty()){
-                        Pair p = q.poll();
-                        if(p.row>=0 && p.row<n &&p.col>=0 && p.col<m && board[p.row][p.col]=='X' && vis[p.row][p.col]!=1){
-                            vis[p.row][p.col]=1;
-                            q.offer(new Pair(p.row+1,p.col));
-                            q.offer(new Pair(p.row-1,p.col));
-                            q.offer(new Pair(p.row,p.col+1));
-                            q.offer(new Pair(p.row,p.col-1));
-                        }
-                    }
                 }
             }
         }
+
         return count;
     }
 }
