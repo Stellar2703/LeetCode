19a3174fd6a62a4f3b02ffdd8cfb61d75c3d46b6
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-06-27 12:23:43 +0530

    Time: 4 ms (61.17%), Space: 45.1 MB (51.86%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -2,16 +2,20 @@ class Solution {
     public int numOfPairs(String[] nums, String target) {
         int n = nums.length;
         int count =0;
+        int tar = target.length();
+        HashMap<String,Integer> mp = new HashMap<>();
         for(int i =0;i<n;i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<n;j++){
-                sb.append(nums[i]);
-                sb.append(nums[j]);
-                if(j!=i && target.equals(sb.toString())){
-                    count++;
-                }
-                sb.setLength(0);
+            int len = nums[i].length();
+
+            if(tar>len && nums[i].equals(target.substring(0,len)) && mp.containsKey(target.substring(len,tar))){
+                count+=mp.get(target.substring(len,tar));
             }
+
+            if(tar>len && nums[i].equals(target.substring(tar-len,tar)) && mp.containsKey(target.substring(0,tar-len))){
+                count+=mp.get(target.substring(0,tar-len));
+            }
+
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         return count;
     }
