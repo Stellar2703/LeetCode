39258f81ac733893c9a399bcc498c14fd00467d8
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-08-11 10:03:42 +0530

    Time: 6 ms (93.18%), Space: 51.6 MB (99.82%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,25 +1,68 @@
+
 class Solution {
 
     List<List<Integer>> ans;
-    HashSet<List<Integer>> hs;
     public void f(int index,int[] nums,List<Integer> temp){
-        if(temp.size()>=2) hs.add(new ArrayList<>(temp));
-        if(index==nums.length) return;
+        if(temp.size()>=2) ans.add(new ArrayList<>(temp));
+        if(index==nums.length) return;  
 
-        if(temp.isEmpty() || temp.get(temp.size()-1)<= nums[index]){
-            temp.add(nums[index]);
-            f(index+1,nums,temp);
-            temp.remove(temp.size()-1);
+        HashSet<Integer> used = new HashSet<>();
+
+        for (int i = index; i < nums.length; i++) {
+
+            // Don't use the same value twice at this recursion level
+            if (used.contains(nums[i])) continue;
+
+            // Maintain non-decreasing order
+            if (!temp.isEmpty() && nums[i] < temp.get(temp.size() - 1)) {
+                continue;
+            }
+
+            used.add(nums[i]);
+
+            temp.add(nums[i]);
+            f(i + 1, nums, temp);
+            temp.remove(temp.size() - 1);
         }
-        f(index+1,nums,temp);
+
     }
     public List<List<Integer>> findSubsequences(int[] nums) {
-        hs = new HashSet<>();
         ans = new ArrayList<>();
         f(0,nums,new ArrayList<>());
-        for(List<Integer> a : hs){
-            ans.add(a);
-        }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+
+//     List<List<Integer>> ans;
+//     HashSet<List<Integer>> hs;
+//     public void f(int index,int[] nums,List<Integer> temp){
+//         if(temp.size()>=2) hs.add(new ArrayList<>(temp));
+//         if(index==nums.length) return;  
+
+//         if(temp.isEmpty() || temp.get(temp.size()-1)<= nums[index]){
+//             temp.add(nums[index]);
+//             f(index+1,nums,temp);
+//             temp.remove(temp.size()-1);
+//         }
+//         f(index+1,nums,temp);
+//     }
+//     public List<List<Integer>> findSubsequences(int[] nums) {
+//         hs = new HashSet<>();
+//         ans = new ArrayList<>();
+//         f(0,nums,new ArrayList<>());
+//         for(List<Integer> a : hs){
+//             ans.add(a);
+//         }
+//         return ans;
+//     }
+// }
