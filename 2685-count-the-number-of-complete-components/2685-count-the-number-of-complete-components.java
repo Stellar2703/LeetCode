3cc2ec687bf3d264e561221c046d149327774b26
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-09-02 09:17:22 +0530

    Time: 11 ms (48.84%), Space: 47.3 MB (95.34%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,48 +1,98 @@
-    class Solution {
-        List<List<Integer>> adj;
-        int [] vis;
-        int times = 1,ans=0;
-        public int dfs(int node){
-            int res = 1;
-            vis[node] = times;
-            for(int edge : adj.get(node)){
-                if(vis[edge]==0){
-                    res += dfs(edge);
-                }
+class Solution {
+    List<List<Integer>> adj;
+    int[] vis;
+    int v = 0,e = 0;
+    private void dfs(int node){
+        vis[node] = 1;
+        v+=1;
+        e+=adj.get(node).size();
+        for(int vertex : adj.get(node)){
+            if(vis[vertex]==0){
+                dfs(vertex);
             }
-            return res;
-
-        }
-        public int countCompleteComponents(int n, int[][] edges) {
-            int[] count = new int[n];
-            vis = new int[n];
-            adj =new ArrayList<>();
-            Boolean isTrue = true;
-            for(int i=0;i<n;i++)adj.add(new ArrayList<>());
-            for(int [] edge :edges){
-                int u = edge[0];
-                int v = edge[1];
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-                count[u]++;
-                count[v]++;
-            }
-
-            for(int i =0;i<n;i++){
-                if(vis[i]==0){
-                    int node = dfs(i);
-                
-                isTrue = true;
-                for(int j =0;j<n;j++){
-                    if(vis[j]==times && node-1 != count[j]){
-                        isTrue = false;
-                    }
-                }
-                if(isTrue)ans++;
-                times++;
-                }
-            }
-            
-            return ans;
         }
     }
+    public int countCompleteComponents(int n, int[][] edges) {
+        vis = new int[n];
+        adj = new ArrayList<>();
+        int ans =0;
+
+        for(int i =0;i<n;i++)adj.add(new ArrayList<>());
+
+        for(int [] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+        }
+
+        for(int i =0;i<n;i++){
+            if(vis[i]==0){
+                v = 0;
+                e = 0;
+                dfs(i);
+
+                int edge = e/2;
+                int vertices = (v*(v-1))/2;
+
+                if(edge == vertices) ans++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+
+
+    // class Solution {
+    //     List<List<Integer>> adj;
+    //     int [] vis;
+    //     int times = 1,ans=0;
+    //     public int dfs(int node){
+    //         int res = 1;
+    //         vis[node] = times;
+    //         for(int edge : adj.get(node)){
+    //             if(vis[edge]==0){
+    //                 res += dfs(edge);
+    //             }
+    //         }
+    //         return res;
+
+    //     }
+    //     public int countCompleteComponents(int n, int[][] edges) {
+    //         int[] count = new int[n];
+    //         vis = new int[n];
+    //         adj =new ArrayList<>();
+    //         Boolean isTrue = true;
+    //         for(int i=0;i<n;i++)adj.add(new ArrayList<>());
+    //         for(int [] edge :edges){
+    //             int u = edge[0];
+    //             int v = edge[1];
+    //             adj.get(u).add(v);
+    //             adj.get(v).add(u);
+    //             count[u]++;
+    //             count[v]++;
+    //         }
+
+    //         for(int i =0;i<n;i++){
+    //             if(vis[i]==0){
+    //                 int node = dfs(i);
+                
+    //             isTrue = true;
+    //             for(int j =0;j<n;j++){
+    //                 if(vis[j]==times && node-1 != count[j]){
+    //                     isTrue = false;
+    //                 }
+    //             }
+    //             if(isTrue)ans++;
+    //             times++;
+    //             }
+    //         }
+            
+    //         return ans;
+    //     }
+    // }
