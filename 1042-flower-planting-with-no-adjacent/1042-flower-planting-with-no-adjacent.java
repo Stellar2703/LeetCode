39ea93aacf3a45a27bdd5e04b5f772b36daf5072
Shashwath V R <159768160+Stellar2703@users.noreply.github.com>
2026-09-17 11:41:38 +0530

    Time: 15 ms (41.51%), Space: 53.2 MB (89.81%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
@@ -1,13 +1,8 @@
 class Solution {
     public int[] gardenNoAdj(int n, int[][] paths) {
         int[] ans = new int[n];
-        List<HashSet<Integer>> choice = new ArrayList<>();
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<n;i++){
-            choice.add(new HashSet<>());
-            adj.add(new ArrayList<>());
-        }
-        int []  avail = {1,2,3,4};
+        for(int i =0;i<n;i++) adj.add(new ArrayList<>());
         for(int [] edge : paths){
             int u = edge[0];
             int v = edge[1];
@@ -15,20 +10,58 @@ class Solution {
             adj.get(v-1).add(u-1);
         }
 
-        for(int node = 0 ;node<n;node++){
+          for(int node = 0 ;node<n;node++){
+            boolean[] used = new boolean[5];
+
+            for(int edge : adj.get(node)){
+                if(ans[edge]!=0){
+                    used[ans[edge]] = true;
+                }
+            }
             for(int flower = 1;flower<=4;flower++){
-                if(!choice.get(node).contains(flower)){
+                if(used[flower]==false){
                     ans[node] = flower;
-                    for(int edge : adj.get(node)){
-                        choice.get(edge).add(flower);
-                    }
                     break;
                 }
             }
         }
         return ans;
+    }
+}
+
+
+// class Solution {
+//     public int[] gardenNoAdj(int n, int[][] paths) {
+//         int[] ans = new int[n];
+//         List<HashSet<Integer>> choice = new ArrayList<>();
+//         List<List<Integer>> adj = new ArrayList<>();
+//         for(int i =0;i<n;i++){
+//             choice.add(new HashSet<>());
+//             adj.add(new ArrayList<>());
+//         }
+//         int []  avail = {1,2,3,4};
+//         for(int [] edge : paths){
+//             int u = edge[0];
+//             int v = edge[1];
+//             adj.get(u-1).add(v-1);
+//             adj.get(v-1).add(u-1);
+//         }
+
+//         for(int node = 0 ;node<n;node++){
+//             for(int flower = 1;flower<=4;flower++){
+//                 if(!choice.get(node).contains(flower)){
+//                     ans[node] = flower;
+//                     for(int edge : adj.get(node)){
+//                         choice.get(edge).add(flower);
+//                     }
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
 
         
         
-    }
-}
+//     }
+// }
+
